128d92bb7142c9023dc68080e17c471facd79b66
sadek <114314659+zrSadek@users.noreply.github.com>
2024-04-08 10:46:11 +0200

    Update client.js

--- utils/client.js
+++ utils/client.js
@@ -81,13 +81,5 @@ class client extends Client {
         return new AttachmentBuilder(f, { name })
     }
 };
-process.on("unhandledRejection", (reason, p) => {
-    console.log(reason, p);
-});
-process.on("uncaughtException", (err, origin) => {
-    console.log(err, origin);
-});
-process.on("multipleResolves", (type, promise, reason) => {
-    console.log(type, promise, reason);
-});
+
 module.exports = client
